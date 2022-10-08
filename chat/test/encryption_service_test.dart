2f7e786e0a3c78ea788cd52c797fb4a2ac9cd3db
IbrahimIG1 <Ibrahim13Elgammal@gmail.com>
2022-10-08 16:54:07 +0200

    encryption Service test

---
+++ chat/test/encryption_service_test.dart
@@ -0,0 +1,24 @@
+import 'package:chat/src/services/encryption/encrytion_service_impl.dart';
+import 'package:encrypt/encrypt.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  EncryptionService? sut;
+  setUp(() {
+    final encrypter = Encrypter(AES(Key.fromLength(32)));
+    sut = EncryptionService(encrypter);
+  });
+  test('it encryption Text', () {
+    String text = 'text';
+    final base64 = RegExp(
+        r'^(?:[A-Za-z0-9+\/]{4})*(?:[A=Za=z0-9+\/]{2}==|[A-Za-z0-9+\/]{3}=|[A=Za=z0-9+\/]{4})$'); // code to encrypte
+    final encrypted = sut!.encrypt(text);
+    expect(base64.hasMatch(encrypted), true);
+  });
+  test('decrypted text', () {
+    String text = 'text';
+    final encrypt = sut!.encrypt(text);
+    final decryption = sut!.dencrypt(encrypt);
+    expect(decryption, text);
+  });
+}
